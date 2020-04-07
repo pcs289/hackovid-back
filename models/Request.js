8c819d22ba6392ce54f8bfcf433acf991836bde4
JoanFlotatsa <joanflotatsa@iqs.edu>
2020-04-07 21:04:38 +0200

    Add new request endpoints

--- models/Request.js
+++ models/Request.js
@@ -8,7 +8,7 @@ const requestSchema = new Schema(
     requester: { type: ObjectId, ref: 'User', required : true }, // Service Consumer can alter status: {1,2} -> 3
     requested: { type: ObjectId, ref: 'User', required : true }, // Service Provider can alter status: 0 -> {1,2}
     preference: { type: Schema.ObjectId, ref: 'User.preferences', required: true }, // One of Requested's Preferences
-    status: { type: Number, required: true, default: 0 }, // Status --> 0 : Requested, 1 : Accepted, 2 : Denied, 3 : Done
+    status: { type: Number, required: true, default: 0 }, // Status --> 0 : Requested, 1 : Accepted, 2 : Denied,  3 : Canceled , 4 : Done 
     description: { type: String, default : null }
   },
   {
