9d903aaf4dddb26bbe8a7cbe9559153bdacc15b6
paudelacuesta <paudelacuesta@ciclion.com>
2020-04-10 22:52:06 +0200

    GeoFix v2

--- routes/map.js
+++ routes/map.js
@@ -20,12 +20,18 @@ router.put(
 });
 
 const toRad = deg => deg * Math.PI / 180;
+const R = 6371;
 const distance = (pos1, pos2) => {
   const x1 = pos2.lat-pos1.lat;
   const dLat = toRad(x1);
   const x2 = pos2.lon-pos1.lon;
   const dLon = toRad(x1);
-  const a = Math.sin(dLat/2) ** 2 + Math.sin(dLon/2) ** 2
+  const a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(toRad(pos1.lat)) * Math.cos(toRad(pos2.lat)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  return R * c;
 };
 
 router.post(
@@ -35,6 +41,7 @@ router.post(
         try {
             const { radius, dayOfWeek } = req.body;
             const myNeighbors = await User.find({
+                "_id": { "$ne": req.session.currentUser._id },
                 "location": {
                     "$near": {
                         "$geometry": req.session.currentUser.location,
@@ -47,7 +54,9 @@ router.post(
                 for (let offerId of neig.offers) {
                     const offer = await Offer.findOne({ _id: offerId });
                     if (offer && offer.dayOfWeek === parseInt(dayOfWeek)) {
-                        offers.push(Object.assign({}, offer._doc, { proximity: , location: neig.location }));
+                        const loc = req.session.currentUser.location;
+                        const proximity = distance({lat: neig.location.coordinates[1], lon: neig.location.coordinates[0]}, {lat: loc.coordinates[1], lon: loc.coordinates[0]});
+                        offers.push(Object.assign({}, offer._doc, { proximity, location: neig.location }));
                     }
                 }
             }
